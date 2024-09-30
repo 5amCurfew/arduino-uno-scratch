827128e09ad23ad5fabf925eb94d4bb6daac9fa5
5amCurfew <samueltobyknight@gmail.com>
2024-09-30 16:20:26 +0100

    comment indentation

--- basics__led_with_switch.ino
+++ basics__led_with_switch.ino
@@ -13,21 +13,21 @@ void loop() {
   //Serial.print(digitalRead(LED_SWITCH_PIN));
 
   /*
-  When the switch is closed, the current primarily flows from the 5V supply to the input pin (connected to the Arduino) 
-  instead of the pull-down resistor because of the path of least resistance. 
+    When the switch is closed, the current primarily flows from the 5V supply to the input pin (connected to the Arduino) 
+    instead of the pull-down resistor because of the path of least resistance. 
 
-  When the switch is open, the input pin LED_SWITCH_PIN is not connected to the 5V supply anymore, leaving the pin "floating" or undefined. 
-  Without the pull-down resistor, the input pin LED_SWITCH_PIN is in a floating state, resulting in unpredictable readings. 
-  The pull-down resistor provides a controlled path for current to flow from the input pin to ground, 
-  ensuring the pin is at a definite LOW (0V) state when the switch is open.
+    When the switch is open, the input pin LED_SWITCH_PIN is not connected to the 5V supply anymore, leaving the pin "floating" or undefined. 
+    Without the pull-down resistor, the input pin LED_SWITCH_PIN is in a floating state, resulting in unpredictable readings. 
+    The pull-down resistor provides a controlled path for current to flow from the input pin to ground, 
+    ensuring the pin is at aß definite LOW (0V) state when the switch is open.
 
-  Switch Closed:
-    The input pin is directly connected to 5V.
-    The pull-down resistor is largely bypassed, and the input pin reads HIGH (1).
-  Switch Open:
-    The input pin is not connected to 5V.
-    The pull-down resistor ensures the input pin is at 0V, by allowing a small current to flow from the input pin through the resistor to ground, 
-    forcing the Arduino to read LOW (0).
+    Switch Closed:
+      The input pin is directly connected to 5V.
+      The pull-down resistor is largely bypassed, and the input pin reads HIGH (1).
+    Switch Open:
+      The input pin is not connected to 5V.
+      The pull-down resistor ensures the input pin is at 0V, by allowing a small current to flow from the input pin through the resistor to ground, 
+      forcing the Arduino to read LOW (0).
   */
 
   if (digitalRead(LED_SWITCH_PIN) == HIGH){
